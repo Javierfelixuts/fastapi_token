149879ed44d22aa291db258e95b230156deacf03
Javierfelixuts <javierfelixuts@gmail.com>
2021-10-26 08:18:56 -0700

    Update DatabaseConection

--- sql_app/database.py
+++ sql_app/database.py
@@ -16,7 +16,7 @@ cloudinary.config(
 #SQLALCHEMY_DATABASE_URL = "postgresql://lruazgbimufqhz:d0706b73cba7e4f40e24773a3fd1dad7ce91370aa1cef7f1d240f44a02f9f29a@ec2-18-209-143-227.compute-1.amazonaws.com:5432/ddrhju6ehvq9vi"
 
 #----- ON LOCAL MACHINE ----
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1191a76ae02f21ed789f3d06431c3ba6919a906de38b8f6508445551f612569f@ec2-54-91-188-254.compute-1.amazonaws.com/d8glm43g7e6qb7"
+SQLALCHEMY_DATABASE_URL = "postgresql://pimfdlwjipaugt:1191a76ae02f21ed789f3d06431c3ba6919a906de38b8f6508445551f612569f@ec2-54-91-188-254.compute-1.amazonaws.com/d8glm43g7e6qb7"
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:J8v5.f675@localhost/granjas_test1"
 
 engine = create_engine(
